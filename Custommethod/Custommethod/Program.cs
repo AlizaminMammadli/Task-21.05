c040bed5e79a439344a1a2730d9d2be501ff7cc9
Alizamin23 <alizaminam@code.edu.az>
2024-05-21 04:32:41 +0400

    Added Task

---
+++ Custommethod/Custommethod/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Custommethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static string MyReplace (string currentValue, string oldValue,  string newValue) 
+        {
+            if (newValue != currentValue)
+            {
+                Console.WriteLine(currentValue);
+            }
+            if (oldValue != currentValue)
+            {
+                Console.WriteLine(currentValue);
+            }
+            return currentValue;
+        }
+
+        static string MySubstring (string myString, int startIndex, int endIndex, int lengthString) 
+        {
+            if (myString != null)
+            {
+                myString = myString.Substring (startIndex, endIndex);
+                return myString;
+            }
+            
+        }
+        static string MyTrim (string myTrimString) 
+        { 
+            
+            
+        }
+
+    }
+}
---
+++ ValidatePassword/ValidatePassword/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ValidatePassword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your password");
+            string password = Console.ReadLine();
+            bool result = ValidatePassword(password);
+            Console.WriteLine(result);
+        }
+
+        static bool ValidatePassword(string password)
+        {
+           if (password.Length < 8) 
+            {
+                return false;
+            }
+           if (!char.IsUpper(password[0]))
+            {
+                return false;
+            }
+           if (!password.Any(char.IsDigit))
+            {
+                return false;
+            }
+           if (!password.Any(char.IsLetterOrDigit))
+            {
+                return false;
+            }
+           return true;
+        }
+    }
+}
